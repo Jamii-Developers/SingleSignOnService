198b34b2137b35193e87f47f2686a2316dc92839
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2023-09-03 18:26:23 -0500

    Adds account reactivation and deactivation Adds UserGroups Adds UserRoles

--- src/main/java/com/jamii/webapi/WebapiApplication.java
+++ src/main/java/com/jamii/webapi/WebapiApplication.java
@@ -8,12 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@CrossOrigin(origins = "*") // Apply to the entire controller
 @SpringBootApplication
 @RestController
 public class WebapiApplication {
