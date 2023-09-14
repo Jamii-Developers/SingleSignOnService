09a984a21903156b18f3abc5b89199ad7a1d5b11
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2023-09-13 19:50:40 -0500

    File Upload completed and tested successfully

--- src/main/java/com/jamii/jamiidb/controllers/FileTableOwnerCONT.java
+++ src/main/java/com/jamii/jamiidb/controllers/FileTableOwnerCONT.java
@@ -1,7 +1,6 @@
 package com.jamii.jamiidb.controllers;
 
 import com.jamii.jamiidb.model.FileTableOwnerTBL;
-import com.jamii.jamiidb.model.UserLoginTBL;
 import com.jamii.jamiidb.repo.FileTableOwnerREPO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
