939d4ea73b22b8b883a26b180484f60db2bcd55a
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2025-05-10 20:45:06 -0500

    Adding the following:
    1. Removing followers
    2. Un-Friending
    3. Un-Following
    4. Un-Blocking
    5. View User Profile

---
+++ src/main/java/com/jamii/operations/userServices/social/ViewUserProfileOPS.java
@@ -0,0 +1,69 @@
+package com.jamii.operations.userServices.social;
+
+import com.jamii.Utils.JamiiMapperUtils;
+import com.jamii.jamiidb.controllers.UserData;
+import com.jamii.jamiidb.controllers.UserLogin;
+import com.jamii.jamiidb.model.UserDataTBL;
+import com.jamii.jamiidb.model.UserLoginTBL;
+import com.jamii.operations.userServices.AbstractUserServicesOPS;
+import com.jamii.requests.userServices.socialREQ.ViewUserProfileServicesREQ;
+import com.jamii.responses.userResponses.socialResponses.ViewUserProfileRESP;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ViewUserProfileOPS extends AbstractUserServicesOPS {
+
+
+    @Autowired
+    private UserLogin userLogin;
+    @Autowired
+    private UserData userData;
+
+    @Override
+    public void validateCookie( ) throws Exception{
+        ViewUserProfileServicesREQ req = ( ViewUserProfileServicesREQ ) JamiiMapperUtils.mapObject( getRequest( ), ViewUserProfileServicesREQ.class );
+        setDeviceKey( req.getDeviceKey( ) );
+        setUserKey( req.getUserKey( ) );
+        setSessionKey( req.getSessionKey() );
+        super.validateCookie( );
+    }
+
+    @Override
+    public void processRequest() throws Exception {
+
+        if( !getIsSuccessful( ) ){
+            return;
+        }
+
+        ViewUserProfileServicesREQ req = ( ViewUserProfileServicesREQ ) JamiiMapperUtils.mapObject( getRequest( ), ViewUserProfileServicesREQ.class );
+
+        // Check if both users exist in the system
+        this.userLogin.data = new UserLoginTBL( );
+        this.userLogin.otherUser = new UserLoginTBL( );
+        this.userLogin.data = this.userLogin.fetchByUserKey( UserKey, UserLogin.ACTIVE_ON ).orElse( null );
+        this.userLogin.otherUser = this.userLogin.fetchByUserKey( req.getTargetUserKey( ), UserLogin.ACTIVE_ON ).orElse( null );
+        if( this.userLogin.data == null  || this.userLogin.otherUser == null ){
+            this.jamiiErrorsMessagesRESP.setGenerateGenericError( );
+            this.JamiiError = jamiiErrorsMessagesRESP.getJSONRESP( ) ;
+            this.isSuccessful = false;
+            return;
+        }
+
+        this.userData.data = new UserDataTBL( );
+        this.userData.data = this.userData.fetch( this.userLogin.otherUser, UserData.CURRENT_STATUS_ON ).orElse( null );
+
+    }
+
+    @Override
+    public ResponseEntity<?> getResponse( ){
+
+        if( getIsSuccessful() ){
+
+            return new ResponseEntity< >( new ViewUserProfileRESP( this.userLogin.otherUser, this.userData.data ), HttpStatus.OK ) ;
+        }
+        return super.getResponse( );
+    }
+}
