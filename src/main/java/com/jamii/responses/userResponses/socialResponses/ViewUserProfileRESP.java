8f5d41beda787be262f3b354ec72bfd4db84f82a
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2025-05-17 17:39:18 -0500

    Updating View User Profile Data

--- src/main/java/com/jamii/responses/userResponses/socialResponses/ViewUserProfileRESP.java
+++ src/main/java/com/jamii/responses/userResponses/socialResponses/ViewUserProfileRESP.java
@@ -13,9 +13,15 @@ public class ViewUserProfileRESP extends AbstractResponses {
         setEmailaddress( user.getEmailaddress( ) );
 
         if( userData != null ) {
-            setFirstname(userData.getFirstname());
-            setMiddlename(userData.getMiddlename());
-            setLastname(userData.getLastname());
+            setFirstname( userData.getFirstname( ) );
+            setMiddlename( userData.getMiddlename( ) );
+            setLastname( userData.getLastname( ) );
+            setAddress1( userData.getAddress1( ) );
+            setAddress2( userData.getAddress2( ) );
+            setCity( userData.getCity( ) );
+            setState( userData.getState( ) );
+            setCountry( userData.getCountry( ) );
+            setZipcode(userData.getZipcode( ) );
         }
     }
 
@@ -24,6 +30,13 @@ public class ViewUserProfileRESP extends AbstractResponses {
     private String firstname;
     private String middlename;
     private String lastname;
+    private String address1;
+    private String address2;
+    private String city;
+    private String province;
+    private String state;
+    private String country;
+    private String zipcode;
 
     public String getUsername() {
         return username;
@@ -63,5 +76,61 @@ public class ViewUserProfileRESP extends AbstractResponses {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 }
