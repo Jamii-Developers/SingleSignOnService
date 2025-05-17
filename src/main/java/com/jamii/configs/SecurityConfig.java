e3a7d52cc4c3aa6c67586fe026c3b03abb052588
michael-ayako <56703374+michael-ayako@users.noreply.github.com>
2025-05-17 15:40:36 -0500

    Updating to allow public to be accessible to all

--- src/main/java/com/jamii/configs/SecurityConfig.java
+++ src/main/java/com/jamii/configs/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/health").permitAll( )
-                        .requestMatchers("/**").permitAll( )
+                        .requestMatchers("/api/public/**").permitAll( )
                         .anyRequest().authenticated()
                 );
 
